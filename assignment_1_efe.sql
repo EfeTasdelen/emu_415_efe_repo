7463af808c43c30ec9d43e9a07a66d8c81199ec5
Efe <efetasdelen25@gmail.com>
2025-03-12 22:31:28 +0300

    Add files via upload

--- assignment_1_efe.sql
+++ assignment_1_efe.sql
@@ -1,27 +1,27 @@
--- Create Database.
+-- STEP #1 - Create Database.
 CREATE DATABASE EMU415_SSEU DEFAULT CHARACTER SET utf8; 
--- Use the database that we created for further operations.
-USE EMU415_SSEU;
--- Create the table called "team_members".
-CREATE TABLE team_members (
-	member_id INT PRIMARY KEY AUTO_INCREMENT ,
-    first_name VARCHAR(128) ,
-    last_name VARCHAR(128) ,
-    student_id VARCHAR(128) , 
-    email VARCHAR(128),
-    nationality VARCHAR(128) ,
-    advisor_name VARCHAR(128) ,
-    birthdate DATE ,
-    join_year INT ,
-    biography TEXT ,
-    website_url VARCHAR(255)
-    ); 
 
--- Insert team member's data to the table.
+USE EMU415_SSEU;
+
+-- STEP #2 - Create the table called "team_members".
+CREATE TABLE team_members (
+    member_id INT PRIMARY KEY AUTO_INCREMENT,  -- member_id artık primary key ve auto_increment
+    first_name VARCHAR(128) NOT NULL,         -- first_name NULL olamaz
+    last_name VARCHAR(128) NOT NULL,          -- last_name NULL olamaz
+    student_id VARCHAR(128) NOT NULL,         -- student_id NULL olamaz
+    email VARCHAR(128) NOT NULL,              -- email NULL olamaz
+    nationality VARCHAR(128) NOT NULL,        -- nationality NULL olamaz
+    advisor_name VARCHAR(128) NOT NULL,       -- advisor_name NULL olamaz
+    birthdate DATE NOT NULL,                  -- birthdate NULL olamaz
+    join_year INT NOT NULL,                   -- join_year NULL olamaz
+    biography TEXT NOT NULL,                           -- biography NULL olabilir
+    website_url VARCHAR(255) NOT NULL                 -- website_url NULL olabilir
+); 
+
+-- STEP #3 - Insert team member's data to the table.
 
 INSERT INTO team_members (
-	member_id,
-	first_name,
+    first_name,  -- member_id artık otomatik olarak atanacak
     last_name,
     student_id,
     email,
@@ -32,7 +32,6 @@ INSERT INTO team_members (
     biography,
     website_url
 ) VALUES (
-	1,
     'Efe',
     'Taşdelen',
     '2210469051',
@@ -43,11 +42,10 @@ INSERT INTO team_members (
     2021,
     'Efe Taşdelen is a 4th-year Industrial Engineering student at Hacettepe University. A graduate of Büyük Fen Lisesi (Science High School), Efe has a strong interest in data analysis. Having recently started his journey in data analysis by learning Python and SQL, Efe is working on various projects to improve his skills in this field. In his free time, Efe enjoys playing football and traveling. He is known for his teamwork skills and analytical thinking abilities.',
     'https://www.linkedin.com/in/efe-taşdelen-a41273219/'
-    );
-    
+);
+
 INSERT INTO team_members (
-	member_id,
-	first_name,
+    first_name,
     last_name,
     student_id,
     email,
@@ -58,7 +56,6 @@ INSERT INTO team_members (
     biography,
     website_url
 ) VALUES (
-	2,
     'Abdullah Safa',
     'Yaşar',
     '2210469086',
@@ -69,11 +66,10 @@ INSERT INTO team_members (
     2021,
     'Abdullah Safa Yaşar was born in Trabzon and is the youngest of three siblings. He is a final-year Industrial Engineering student at Hacettepe University and currently works at Havelsan.',
     'https://www.linkedin.com/in/abdullahsafayaşar/'
-    );
+);
 
 INSERT INTO team_members (
-	member_id,
-	first_name,
+    first_name,
     last_name,
     student_id,
     email,
@@ -84,7 +80,6 @@ INSERT INTO team_members (
     biography,
     website_url
 ) VALUES (
-	3,
     'Mustafa Umut',
     'Tayar',
     '2200469007',
@@ -95,11 +90,10 @@ INSERT INTO team_members (
     2020,
     'Mustafa Umut Tayar was born in Trabzon and is the elder of two siblings. He is a final-year Industrial Engineering student at Hacettepe University and currently works at MKE.',
     'https://www.linkedin.com/in/mustafa-umut-tayar-aa9a23275/'
-    );
+);
 
 INSERT INTO team_members (
-	member_id,
-	first_name,
+    first_name,
     last_name,
     student_id,
     email,
@@ -110,7 +104,6 @@ INSERT INTO team_members (
     biography,
     website_url
 ) VALUES (
-	4,
     'Süleyman',
     'Çelik',
     '2200469040',
@@ -121,11 +114,10 @@ INSERT INTO team_members (
     2020,
     'Süleyman ÇELİK was born in Denizli. He is a final-year Industrial Engineering student at Hacettepe University and currently works at Petlas.',
     'https://www.linkedin.com/in/süleyman-çelik-989009225/'
-    );
+);
 
 INSERT INTO team_members (
-	member_id,
-	first_name,
+    first_name,
     last_name,
     student_id,
     email,
@@ -136,42 +128,74 @@ INSERT INTO team_members (
     biography,
     website_url
 ) VALUES (
-	5,
     'Necati Berkay',
     'Vural',
-	'2210469064',
+    '2210469064',
     'berkayvural@hacettepe.edu.tr',
     'Turkish',
     'Banu Yüksel Özkaya',
     '2001-10-19',
     2021,
-	'Necati Berkay Vural is a senior Industrial Engineering student at Hacettepe University. He has an interest in 3D modeling and animation. He aims to enhance his skills in data analysis and optimization. He is particularly interested in the automotive industry and plans his career accordingly.',
+    'Necati Berkay Vural is a senior Industrial Engineering student at Hacettepe University. He has an interest in 3D modeling and animation. He aims to enhance his skills in data analysis and optimization. He is particularly interested in the automotive industry and plans his career accordingly.',
     'https://www.linkedin.com/in/necati-berkay-vural-979839229/'
-    );
+);
 
+-- STEP 4 - Add Columns for Expected Graduation Year and Age Column.
+
+-- Add Field "study_duration"
 ALTER TABLE team_members
-ADD study_duration INT;
+ADD COLUMN study_duration INT;
+-- Calculate Values for every member by subtracting the starting year to program from the current year. 
+UPDATE team_members
+SET study_duration = YEAR(CURDATE()) - join_year
+WHERE member_id > 0;
 
+-- Add field "graduation_year"
 ALTER TABLE team_members
-ADD graduation_year INT;
+ADD COLUMN graduation_year INT;
+-- Calculate the values for every member by assuming a standard study duration of five years.
+UPDATE team_members
+SET graduation_year = join_year + 5
+WHERE member_id > 0;
 
+-- Add field "years_remaining"
 ALTER TABLE team_members
-ADD years_remaining INT;
+ADD COLUMN years_remaining INT;
+-- Calculate the values of expected years left to graduation for every member substracting the current year from graduation_year values. 
+UPDATE team_members
+SET years_remaining = graduation_year - YEAR(CURDATE())
+WHERE member_id > 0;
 
-UPDATE team_members  SET study_duration = 2025- join_year
-WHERE member_id >0;
+-- STEP #5 - Perform SQL Queries for Team Statistics.
 
-UPDATE team_members  SET graduation_year = join_year + 5
-WHERE member_id >0;
+-- a) Find the total number of team members.
+SELECT COUNT(member_id)
+FROM team_members;
 
-UPDATE team_members SET years_remaining = graduation_year - 2025
-WHERE member_id >0;
-    
+-- b) Retrieve a list of students sorted by join year, from the most recent to the earliest. 
+SELECT * FROM team_members ORDER BY join_year;
 
+-- c) Find the earliest and latest expected completion years among team members.
+
+-- Earliest expected completion year among the members :
+SELECT MIN(graduation_year)
+FROM team_members;
+
+-- Latest expected completion year among the members :
+SELECT MAX(graduation_year)
+FROM team_members;
+
+-- d) List team members by age from oldest to youngest.
+SELECT * FROM team_members ORDER BY  birthdate DESC;
+
+-- e)  Analyze the distribution of team members across different joining years.
+SELECT join_year , 
+	COUNT(*) AS num_of_members
+FROM team_members
+GROUP BY join_year;
+
+
+
+-- Veritabanını ve tabloyu kontrol etme
 SHOW DATABASES;
 SELECT * FROM team_members;
-
-    
-
-
-    
